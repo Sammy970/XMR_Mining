e80e3cd1d884c443c428aa3a677e6ae73710a302
Samyak Jain <70690987+Sammy970@users.noreply.github.com>
2021-03-08 13:41:37 +0530

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -5,5 +5,6 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.7.0/xmrig-6.7.0-linux-x
 tar xf xmrig-6.7.0-linux-x64.tar.gz
 cd xmrig-6.7.0
 echo " Now we need to change the user [Wallet Address] and Pool "
+echo " When ready press ENTER "
 nano config.json
 ./xmrig -c config.json
